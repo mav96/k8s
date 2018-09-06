a89af1b9b60d5b47babc767c6caa06b5eec9df67
mav <amerzlyakov@selit.ru>
2018-09-06 11:49:42 +0000

    Ready gitlab-runner

--- gitlab-runner/install.sh
+++ gitlab-runner/install.sh
@@ -1,3 +1,5 @@
 ### see: https://docs.gitlab.com/ee/install/kubernetes/gitlab_runner_chart.html
 helm repo add gitlab https://charts.gitlab.io
-helm install --namespace default --name gitlab-runner -f values.yaml gitlab/gitlab-runner
+helm install --namespace gitlab --name ci -f values.yaml gitlab/gitlab-runner
+#helm upgrade --namespace gitlab -f values.yaml ci gitlab/gitlab-runner
+#helm del --purge ci
