91f4f436948b6ca052e2affdcf2c93e04d4708d1
mav <amerzlyakov@selit.ru>
2018-09-12 16:06:07 +0000

    Add tls in traefik

---
+++ traefik/install.sh
@@ -0,0 +1,4 @@
+kubectl create secret generic traefik-cert --from-file=tls.crt --from-file=tls.key -n kube-system
+kubectl create configmap traefik-conf --from-file=traefik.toml -n kube-system
+kubectl apply -f traefik-rs.yaml
+
