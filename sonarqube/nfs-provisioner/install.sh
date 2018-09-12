82c522e75f74870906fa345c474b927ec29918f7
mav <amerzlyakov@selit.ru>
2018-09-12 11:51:29 +0000

    Add readme for nfs

--- sonarqube/nfs-provisioner/install.sh
+++ sonarqube/nfs-provisioner/install.sh
@@ -1,3 +1,10 @@
 ## see: https://github.com/kubernetes-incubator/external-storage/tree/master/nfs
+
+kubectl get nodes
+kubectl label nodes fnc-worker-2  disktype=nfs
+kubectl get nodes --show-labels
+#On NFS node:
+mkdir -p /data
+
 kubectl create -f deployment.yaml
 kubectl create -f class.yaml
