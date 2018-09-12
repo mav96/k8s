dd51dee8e1cbae124629a0e39d20ea3c35d5cab8
mav <amerzlyakov@selit.ru>
2018-09-12 11:38:25 +0000

    Add nfs-provisioner

--- sonarqube/install.sh
+++ sonarqube/install.sh
@@ -1,14 +1,12 @@
 ### see: https://coderise.io/installing-sonarqube-in-kubernetes/
 ######## https://github.com/jonmosco/kubernetes-sonar
 
+# first install nfs-provisioner !!!
+
 kubectl create secret generic postgres-pwd --from-literal=password=P@ssw0rd
 #kubectl create -f sonar-postgres-deployment.yaml  
 #kubectl create -f sonar-postgres-service.yaml  
 kubectl create -f postgres-extrnal-service.yaml
-
-## For all nodes
-sudo mkdir -p /data/sonarqube/extensions
-sudo mkdir -p /data/sonarqube/data
-
-kubectl create -f sonar-pv-pvc.yaml
+kubectl create -f sq-pvc.yaml
+# wait: kubectl get pvc
 kubectl apply -f sonarqube.yaml
---
+++ sonarqube/nfs-provisioner/install.sh
@@ -0,0 +1,3 @@
+## see: https://github.com/kubernetes-incubator/external-storage/tree/master/nfs
+kubectl create -f deployment.yaml
+kubectl create -f class.yaml
