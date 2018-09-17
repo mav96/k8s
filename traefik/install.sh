768f840ed5596e3b56279bf55e230c22dd5dc49f
mav <amerzlyakov@selit.ru>
2018-09-17 15:34:28 +0000

    Add rbac for traefik

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,3 +1,4 @@
+kubectl apply -f traefik-rbac.yaml
 kubectl create secret generic traefik-cert --from-file=tls.crt --from-file=tls.key -n kube-system
 kubectl create configmap traefik-conf --from-file=traefik.toml -n kube-system
 kubectl apply -f traefik-rs.yaml
