9a559ff7338b50b61283a3c7c8813992c787fbe7
mav <amerzlyakov@selit.ru>
2018-09-17 14:12:56 +0000

    Kubeadm print join command

--- master.sh
+++ master.sh
@@ -4,3 +4,6 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+## Show connection string
+#sudo kubeadm token create --print-join-command 
