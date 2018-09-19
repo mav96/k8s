bdb0af6905f1a60800fa78ee796bb9d75ac20126
mav <amerzlyakov@selit.ru>
2018-09-19 11:31:19 +0000

    Helm for dushboard

--- dashboard/install.sh
+++ dashboard/install.sh
@@ -1,6 +1,10 @@
 htpasswd -c ./auth k8s
-
 kubectl create secret generic dashboadr-secret --from-file auth --namespace=kube-system
-kubectl create -f dashboard.yaml
+
+helm upgrade --install dashboard ./dashboard --namespace kube-system 
+## helm delete --purge dashboard
+## kubectl delete  secret dashboadr-secret --namespace=kube-system
+
+
 
 
