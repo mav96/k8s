07863ab4941ef9369b313c0dab516452985b5e1b
mav <amerzlyakov@selit.ru>
2018-09-27 07:55:21 +0000

    Fix

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -3,3 +3,6 @@ helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 #helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring
 helm install --wait --timeout 600 --tiller-connection-timeout 600  coreos/prometheus-operator --name prometheus-operator --namespace monitoring
 helm install coreos/kube-prometheus --name kube-prometheus --set global.rbacEnable=true --namespace monitoring --wait --timeout 1800 --tiller-connection-timeout 1800
+
+
+##see: https://kubernetes.io/docs/tasks/debug-application-cluster/monitor-node-health/
--- sonarqube/nfs-provisioner/install.sh
+++ sonarqube/nfs-provisioner/install.sh
@@ -11,3 +11,7 @@ sudo apt-get install nfs-common
 
 kubectl create -f deployment.yaml
 kubectl create -f class.yaml
+
+
+#Mark a StorageClass as default: https://kubernetes.io/docs/tasks/administer-cluster/change-default-storage-class/
+kubectl patch storageclass selit-nfs -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
